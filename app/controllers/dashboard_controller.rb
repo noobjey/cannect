5af8edfd291d289f019cc61d78dddc36a93898ac
Jason Wright <noobjey@gmail.com>
2015-11-13 20:38:04 -0700

    group returns owner info

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
 
   def index
-    
+    @group = Group.first
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ActiveRecord::Base
 
   has_many :group_users
   has_many :users, through: :group_users
+
+  def owner
+    User.find_by(id: self.owner_id)
+  end
 end
