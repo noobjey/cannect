46fc58eb5816121b6ae0ea3b28beefd7ef2c8733
Jason Wright <noobjey@gmail.com>
2015-11-15 12:53:13 -0700

    unfollow a group

--- app/controllers/groups/follows_controller.rb
+++ app/controllers/groups/follows_controller.rb
@@ -1,17 +1,24 @@
 class Groups::FollowsController < ApplicationController
 
   def update
-    find_group().users.each do |user|
-      current_user.follow(user)
-    end
+    members.each { |member| current_user.follow(member) }
 
     redirect_to dashboard_path
   end
 
+  def destroy
+    members.each { |member| current_user.unfollow(member) }
+
+    redirect_to dashboard_path
+  end
 
   private
 
   def find_group
     Group.find_by(id: params[:group_id])
+  end
+
+  def members
+    find_group().users
   end
 end
