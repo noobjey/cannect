c1416f9b2d9d41d6c4bc0a0a4728588c49dd9c7f
Jason Wright <noobjey@gmail.com>
2015-11-15 10:41:53 -0700

    refactored follow and unfollow because it was breaking follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,31 @@
 class UsersController < ApplicationController
 
-  def update
-    user_to_follow = User.find_by(id: params[:id])
-    current_user.follow(user_to_follow)
+  def follow
+    if follow?
+      current_user.follow(find_user())
+    elsif unfollow?
+      current_user.unfollow(find_user())
+    end
 
     redirect_to dashboard_path
   end
 
-  def unfollow
-    user_to_unfollow = User.find_by(id: params[:user_to_unfollow])
-    current_user.unfollow(user_to_unfollow)
 
-    redirect_to dashboard_path
+  private
+
+  def find_user
+    User.find_by(id: params[:id])
+  end
+
+  def unfollow?
+    http_method == "delete"
+  end
+
+  def follow?
+    http_method == "put" || http_method == "patch"
+  end
+
+  def http_method
+    params["_method"]
   end
 end
