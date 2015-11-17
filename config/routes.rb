dfc45de6b2b1c1311f270e9d3b0019442133df3d
Jason Wright <noobjey@gmail.com>
2015-11-16 21:51:49 -0700

    twitter and github omniauth login start

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  get '/auth/github', as: :login
-  get '/auth/github/callback', to: 'sessions#create'
+  get '/auth/github', as: :github_login
+  # get '/auth/github/callback', to: 'sessions#create'
+  get '/auth/twitter', as: :twitter_login
+  # get '/auth/twitter/callback', to: 'twitter#create'
+
   delete 'logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#index'
@@ -11,8 +14,17 @@ Rails.application.routes.draw do
     resource :follows, module: :groups, only: [:update, :destroy]
   end
 
+  # make follows a concern
+  # concern :commentable do
+  #   resources :comments
+  # end
+  # resources :messages, concerns: :commentable
+
+
   resources :users, only: [:update] do
     resource :follows, module: :users, only: [:update, :destroy]
   end
+
+  get '/auth/:provider/callback', to: 'sessions#create'
 
 end
