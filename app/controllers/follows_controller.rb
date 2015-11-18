74df70b9a10d0d2b1459c9edeaf465515ccd2dfa
Jason Wright <noobjey@gmail.com>
2015-11-18 13:14:04 -0700

    do not follow yourself when you follow a group

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,26 +1,31 @@
 class FollowsController < ApplicationController
   def create
-    allowed_params.each do |user_id_only_used_for_grouping, data|
+    allowed_params.each do |user_id, data|
       data.each do |service, data|
-        ServiceFactory.create_service(current_user, service).follow(user_to_follow(data, service))
+        ServiceFactory.create_service(current_user, service).follow(user_to_follow(data, service)) unless following_yourself?(user_id)
       end
     end
+
 
     redirect_to dashboard_path
   end
 
-
   def destroy
-    allowed_params.each do |user_id_only_used_for_grouping, data|
+    allowed_params.each do |user_id, data|
       data.each do |service, data|
-        ServiceFactory.create_service(current_user, service).unfollow(user_to_follow(data, service))
+        ServiceFactory.create_service(current_user, service).unfollow(user_to_follow(data, service)) unless following_yourself?(user_id)
       end
     end
+
     redirect_to dashboard_path
   end
 
 
   private
+
+  def following_yourself?(id)
+    current_user == User.find_by(id: id)
+  end
 
   def user_to_follow(data, service)
     Authorization.find_by_provider_and_uid(service, data[:user_to_follow_uid]).username
@@ -31,6 +36,7 @@ class FollowsController < ApplicationController
   end
 
   # allowed params that worked with user button
+  # https://github.com/rails/rails/issues/9454
   # def allowed_params
   #   params.require(:follows).permit(github: :user_to_follow_uid, twitter: :user_to_follow_uid )
   # end
